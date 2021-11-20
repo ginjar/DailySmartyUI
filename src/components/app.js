ee083ae22a2c777312e4681bc5785a9bc0fa8c67
Gina <gkjarvis@mail.fhsu.edu>
2021-11-20 14:43:08 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Gina Daily Smarty</h1>
+        <h2>DevCamp Bottega Course</h2>
+      </div>
+    );
+  }
+}
