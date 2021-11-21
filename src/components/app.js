2888e599eb2621cc43bad3ceacfbe87133e7f034
Gina <gkjarvis@mail.fhsu.edu>
2021-11-21 13:06:50 -0700

    added the logo and styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
+      <div>
         <h1>Gina Daily Smarty</h1>
         <h2>DevCamp Bottega Course</h2>
-      </div>
+<Logo/>
+      
+        </div>
+        
+       
     );
   }
 }
